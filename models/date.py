ba71d56ffe97c33de89d74ffdf00040e4fa600d8
sarahwonago <sarahshaaleh@gmail.com>
2024-03-10 16:47:31 +0300

    date

--- models/date.py
+++
@@ -1,8 +0,0 @@
-class Base:
-    def __init__(self, *args):
-        
-        print("Hello world")
-
-my_name = Base()
-my_name.location = "Kenya"
-print("{} I am from {}".format(my_name, my_name.location))
